a2b9fb0a26f772522868de36244c8e8d2d67e156
Lehakaml <lehakamluk@gmail.com>
2022-07-05 20:13:58 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+
+    let mod = Math.abs(n);
+     
+    let x = mod.toString().split('').map(Number).reverse().join('');
   
-}
+    x = Number(x);
+  
+    return x
+  }
+  
